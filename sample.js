3320873de23da320246f1b634f7509bf239d2950
BitYoungjae <bityoungjae@gmail.com>
2020-03-23 00:32:41 +0900

    add LinkedList sorting sample

--- sample.js
+++ sample.js
@@ -4,6 +4,7 @@ import {
   NumberCollection,
   sort,
   lazySort,
+  LinkedList,
 } from './build/index.js';
 
 const sample = makeSampleArray(20, 1, 1000);
@@ -20,3 +21,16 @@ sort(text, 'descending');
 const lazyText = lazySort(text, 'ascending');
 console.log(text.data);
 console.log([...lazyText].join(''));
+
+const linked = new LinkedList();
+linked.add('b');
+linked.add('a');
+linked.add('d');
+linked.add('c');
+linked.add('e');
+
+const lazyLinked = lazySort(linked, 'ascending');
+console.log([...lazyLinked]);
+
+sort(linked, 'descending');
+console.log(Array.from(linked.data));
