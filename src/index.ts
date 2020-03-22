852385788deb802444ff55e6d81332709b691b83
BitYoungjae <bityoungjae@gmail.com>
2020-03-22 22:57:35 +0900

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,4 @@ import { NumberCollection } from './utils/collections/NumberCollection.js';
 import { makeSampleArray } from './utils/utils.js';
 import { StringCollection } from './utils/collections/StringCollection.js';
 
-const sample = makeSampleArray(20, 1, 1000);
-
-const text = new StringCollection('lkjhgfdsapoiuytrewqmnbvcxz');
-const numbers = new NumberCollection(sample);
-
-sort(numbers, 'descending');
-const lazy = lazySort(numbers, 'ascending');
-console.log(numbers.data);
-console.log([...lazy]);
-
-sort(text, 'descending');
-const lazyText = lazySort(text, 'ascending');
-console.log(text.data);
-console.log([...lazyText].join(''));
+export { sort, lazySort, NumberCollection, StringCollection, makeSampleArray };
